3b67249de11702a2b71993f9def73430e49a5091
Renato <renatoaugustosa@gmail.com>
2025-05-14 19:49:18 -0300

    Criando Models Usuario e Produto

---
+++ LojaBDS/Models/Prdouto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LojaBDS.Models
+{
+    public class Prdouto
+    {
+
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public decimal PrecoProduto { get; set; }
+        public int QuantidadeProduto { get; set; }
+
+    }
+}
---
+++ LojaBDS/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using LojaBDS.Models;
+
+namespace LojaBDS.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NomeUsuario { get; set; }
+        public string EmailUsuario { get; set; }
+        public string SenhaUsuario { get; set; }
+    }
+}
+
+
