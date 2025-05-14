406f41498d693232cce7bafdc245cc99b29adefb
Renato <renatoaugustosa@gmail.com>
2025-05-14 19:27:53 -0300

    Criando o Projeto Loja BDS

---
+++ LojaBDS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LojaBDS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
