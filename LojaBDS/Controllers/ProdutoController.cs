99e65b454e706d716e110f93d087b789ea097463
Renato <renatoaugustosa@gmail.com>
2025-05-15 21:02:07 -0300

    Codificando a ProdutoController

--- LojaBDS/Controllers/ProdutoController.cs
+++ LojaBDS/Controllers/ProdutoController.cs
@@ -1,14 +1,83 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LojaBDS.Models;
 using LojaBDS.Repositorio;
+using MySqlX.XDevAPI;
+using MySql.Data.MySqlClient;
 
 namespace LojaBDS.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly ProdutoController _produtoRepositorio;
+
+
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
         public IActionResult Index()
         {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+
+        public IActionResult CadastrarProduto()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produto produto)
+        {
+
+            _produtoRepositorio.Cadastrar(produto);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditarProduto(int id)
+        {
+            var produto = _produtoRepositorio.ObterProduto(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult Atualizar(int id, [Bind("idProduto, nomeProduto, descricaoProduto, precoProduto, quantidadeProduto")] Produto produto)
+        {
+            if (id != produto.idProduto)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_produtoRepositorio.Atualizar(produto))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Ocorreu um erro ao Editar.");
+                    return View(produto);
+                }
+            }
+            return View(produto);
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
